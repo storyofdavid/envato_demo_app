5b3c33006080489b4f3c49fa26a1fb058e93761d
storyofdavid <ec2-user@ip-172-31-54-185.ec2.internal>
2019-08-27 05:31:50 +0000

    Added students resource, added in validation for students. Added in validation for email address.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190826070119) do
+ActiveRecord::Schema.define(version: 20190826214407) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "short_name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20190826070119) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
