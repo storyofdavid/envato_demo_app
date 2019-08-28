4327867d6331310a226e6964bd5792458adfafc2
storyofdavid <ec2-user@ip-172-31-54-185.ec2.internal>
2019-08-28 10:55:53 +0000

    Set up database to securely store encrpyted user passwords

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190826214407) do
+ActiveRecord::Schema.define(version: 20190828103527) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "short_name"
@@ -23,8 +23,9 @@ ActiveRecord::Schema.define(version: 20190826214407) do
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
