b46500f6e8877600e1fe1370a9bd9ae8a090e082
storyofdavid <ec2-user@ip-172-31-54-185.ec2.internal>
2019-08-29 04:11:05 +0000

    Added back-end functionality to handle student enrollments.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190828103527) do
+ActiveRecord::Schema.define(version: 20190829035559) do
 
   create_table "courses", force: :cascade do |t|
     t.string   "short_name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 20190828103527) do
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "student_courses", force: :cascade do |t|
+    t.integer  "student_id"
+    t.integer  "course_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "students", force: :cascade do |t|
