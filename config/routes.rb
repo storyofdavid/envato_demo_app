e1994ec15112018914708b0be018e1b2003d2b99
storyofdavid <ec2-user@ip-172-31-54-185.ec2.internal>
2019-08-29 21:31:29 +0000

    Added ability to enroll in courses. Created student profile page.

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ resources :students, except: [:destroy]
 get 'login', to: 'logins#new'
 post 'login', to: 'logins#create'
 delete 'logout', to: 'logins#destroy'
+post 'course_enroll', to: 'student_courses#create'
 end
